e600a3a67b417c35836c8d32cbf9285438a9fd1e
Florian Novak <f-novak@gmx.net>
2023-06-21 12:57:22 +0200

    fetch data

--- components/card/card.js
+++ components/card/card.js
@@ -1,29 +1,28 @@
-export function createCharacterCard() {
+export function createCharacterCard(character) {
   const newCard = document.createElement("li");
   newCard.classList.add("card");
-  const cardContainer = document.querySelector('[data-js="card-container"]');
 
   newCard.innerHTML = `<li class="card">
   <div class="card__image-container">
     <img
       class="card__image"
-      src=${image}
+      src=${character.image}
       alt="Rick Sanchez"
     />
     <div class="card__image-gradient"></div>
   </div>
   <div class="card__content">
-    <h2 class="card__title">${name}</h2>
+    <h2 class="card__title">${character.name}</h2>
     <dl class="card__info">
       <dt class="card__info-title">Status</dt>
-      <dd class="card__info-description">${status}</dd>
+      <dd class="card__info-description">${character.status}</dd>
       <dt class="card__info-title">Type</dt>
-      <dd class="card__info-description">${type}</dd>
+      <dd class="card__info-description">${character.type}</dd>
       <dt class="card__info-title">Occurrences</dt>
-      <dd class="card__info-description">${episode.length}</dd>
+      <dd class="card__info-description">${character.episode.length}</dd>
     </dl>
   </div>
 </li>`;
-  cardContainer.append(newCard);
+
   return newCard;
 }
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { createCharacterCard } from "./components/card/card.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +14,24 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+async function fetchCharacters() {
+  try {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await response.json();
+    const results = data.results;
+    console.log(results);
+    console.log(data);
+    // create Card
+    cardContainer.innerHTML = "";
+    // teilschritte 1. append 2. create Character Card 3. was kommt in die Funktion rein
+    // 4. wie werden mehrere Karten erstellt? 5.
+    results.forEach((character) => {
+      const newCard = createCharacterCard(character);
+      cardContainer.append(newCard);
+    });
+  } catch (error) {
+    console.log("Da ist ein Fehler aufgetreten", error);
+  }
+}
+fetchCharacters();
